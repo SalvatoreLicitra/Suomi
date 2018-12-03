57e9e172dce8705c4d0667dd0c1c7df53c3564b7
Salvatore Licitra <salvolicitra95@gmail.com>
2018-12-04 00:08:37 +0100

    project update

---
+++ SUOMI/UserData.aspx.cs
@@ -0,0 +1,55 @@
+﻿using SUOMI.BE;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SUOMI
+{
+
+
+    public partial class UserData : System.Web.UI.Page
+    {
+
+        public static SerialPort mySerialPort { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            if (IsPostBack)
+            {
+
+            }
+
+        }
+
+        protected void btnNext_Click1(object sender, EventArgs e)
+        {
+            BE.User.UtenteCorrente.Age = int.Parse(txtAge.Text);
+            BE.User.UtenteCorrente.Gender = txtGender.Text;
+            BE.User.UtenteCorrente.Height = double.Parse(txtHeight.Text);
+            BE.User.UtenteCorrente.Weight = double.Parse(txtWeight.Text);
+            BE.User.UtenteCorrente.Heart_rate = int.Parse(txtHeartRate.Text);
+            BE.User.UtenteCorrente.BMI = double.Parse(txtWeight.Text) /
+                ((double.Parse(txtHeight.Text) / 100) * (double.Parse(txtHeight.Text) / 100));
+            BLL.GestioneUsers.InsertUserData(BE.User.UtenteCorrente);
+            Response.Redirect("Homepage.aspx");
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string regex = @"[0-9]{2,3}";
+            SerialPort mySerialPort = new SerialPort("COM3", 115200, Parity.Even, 7, StopBits.One) { RtsEnable = true };
+            mySerialPort.Open();
+            String value = SerialPort_Test.HeartRatePulseSensor.LeggiValore(regex, mySerialPort);
+            mySerialPort.Close();
+            SUOMI.BE.User.STM32 = Int32.Parse(value);
+            txtHeartRate.Text = BE.User.STM32.ToString();
+        }
+    }
+}
---
+++ SUOMI/UserData.aspx.designer.cs
@@ -0,0 +1,123 @@
+﻿//------------------------------------------------------------------------------
+// <generato automaticamente>
+//     Codice generato da uno strumento.
+//
+//     Le modifiche a questo file possono causare un comportamento non corretto e verranno perse se
+//     il codice viene rigenerato. 
+// </generato automaticamente>
+//------------------------------------------------------------------------------
+
+namespace SUOMI {
+    
+    
+    public partial class UserData {
+        
+        /// <summary>
+        /// Controllo lblHeartRate.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblHeartRate;
+        
+        /// <summary>
+        /// Controllo txtHeartRate.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtHeartRate;
+        
+        /// <summary>
+        /// Controllo Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
+        /// Controllo lblGender.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblGender;
+        
+        /// <summary>
+        /// Controllo txtGender.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtGender;
+        
+        /// <summary>
+        /// Controllo lblAge.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblAge;
+        
+        /// <summary>
+        /// Controllo txtAge.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtAge;
+        
+        /// <summary>
+        /// Controllo lblHeight.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblHeight;
+        
+        /// <summary>
+        /// Controllo txtHeight.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtHeight;
+        
+        /// <summary>
+        /// Controllo lblWeight.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblWeight;
+        
+        /// <summary>
+        /// Controllo txtWeight.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtWeight;
+        
+        /// <summary>
+        /// Controllo btnNext.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnNext;
+    }
+}
